13abcdd0b95fc763d4e8c9d9211423355031b332
Kyle Danny <codingkyle92@gmail.com>
2021-01-25 23:07:08 +0100

    adding first test ruby file with code

---
+++ basics.rb
@@ -0,0 +1,17 @@
+
+
+def tomorrow
+  tomorrow = 26
+  return tomorrow
+end
+
+puts "you are #{tomorrow} years old"
+
+
+def say_hi(name)
+  return "Hi #{name.length}."
+end
+
+puts say_hi("kYLe")
+
+
